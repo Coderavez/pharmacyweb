fc4595918ff2d5eb0cfffd7e8fd0593a945cb62a
Dmitriy <ml4roff@gmail.com>
2026-05-16 19:25:26 +0300

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,16 +1,19 @@
 <?php
 // db_connection.php
-$servername = "127.0.0.1";
+$host = "db";
 $username = "root";
-$password = "";
+$password = "root";
 $dbname = "apteka";
 
 try {
-    $pdo = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+        $username,
+        $password
+    );
+
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
 ?>
-
-
